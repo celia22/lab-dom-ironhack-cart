c1bfe585ac5971d122a58cb6c85e1ed79b0995d3
celia22 <celialopez22@gmail.com>
2021-03-07 21:18:41 +0100

    finish iteration 2, start iteration 3

--- js/index.js
+++ js/index.js
@@ -9,16 +9,39 @@ function updateSubtotal(x) {
   
 }
 
+console.log("hello")
+
 
 function calculateAll() {
   
- const updateAllProducts = document.getElementsByClassName('.product');
+ let updateAllProducts = document.getElementsByClassName("product");
+ //let totalSum = document.getElementById("total-value span").innerHTML = `${finalValue}`// Aun por declarar finalvalue y hacer la suma
+ 
+ let finalValue = 0;
+ for(let x = 0; x < updateAllProducts.length; x++){   
+    updateSubtotal(updateAllProducts[x]);
+   
+    console.log(x)
+ }; 
 
- updateAllProducts.forEach(x => updateSubtotal(x));
+
+
+/*
+Our calculation functionality is still incomplete. The subtotal for each product is being updated, 
+but the total value remains untouched.
+
+At the end of the `calculateAll()` function, reuse the total value you just calculated in the previous
+ iteration and update the corresponding DOM element. Calculate the total price for the products in your
+  cart by summing all of the subtotals returned by `updateSubtotal()` when it was called with each product.
+
+Lastly, display that value on your DOM.*/
+
 
   // ITERATION 3
   //... your code goes here
 }
+
+calculateAll()
 
 // ITERATION 4
 
